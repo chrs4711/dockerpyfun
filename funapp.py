bba644a66a6ca783d42d79d71589e43afde90b4e
Christian Wander <cwshit@posteo.de>
2020-12-13 19:59:08 +0100

    printing more information

--- funapp.py
+++ funapp.py
@@ -2,7 +2,7 @@ import sys
 
 python_version = sys.version
 
-print(f'>> This is an example app running {python_version}')
+print(f'>> This is an example app running Python {python_version}')
 
 print(f'>> Number of arguments: {len(sys.argv)}')
 print(f'>> Arguments: {str(sys.argv)}')
